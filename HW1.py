b2242231ea02355ce24d094be6eec710358416a7
swollow <swollow@ee.evoplay.tech>
2023-07-26 17:00:20 +0300

    ver1

---
+++ HW1.py
@@ -0,0 +1,8 @@
+print(sum([i for i in range(4)])) # first
+
+print(set([1, 1, 2, 2, 3, 3, 4, 4, 5, 5])) # second
+
+employe_info1 = {"name":"Dima","surname":"Bezsonov","salary": "500"} #third
+print(employe_info1) #trainee
+employe_info1["salary"] = "1500" # after one year
+print(employe_info1) # good job
