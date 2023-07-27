457f9c96bfdba29911f65fa5a69998c67e76f112
swollow <swollow@ee.evoplay.tech>
2023-07-27 10:55:40 +0300

    ver 2

--- HW1.py
+++ HW1.py
@@ -1,6 +1,6 @@
 print(sum([i for i in range(4)])) # first
 
-print(set([1, 1, 2, 2, 3, 3, 4, 4, 5, 5])) # second
+print(sum(set([1, 1, 2, 2, 3, 3, 4, 4, 5, 5]))) # second
 
 employe_info1 = {"name":"Dima","surname":"Bezsonov","salary": "500"} #third
 print(employe_info1) #trainee
