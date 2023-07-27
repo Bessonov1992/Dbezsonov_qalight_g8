f106ba995c4ce7f4c6f35c1b793a1e5346c1bddc
swollow <swollow@ee.evoplay.tech>
2023-07-27 11:00:34 +0300

    v3

--- HW1.py
+++ HW1.py
@@ -2,7 +2,7 @@ print(sum([i for i in range(4)])) # first
 
 print(sum(set([1, 1, 2, 2, 3, 3, 4, 4, 5, 5]))) # second
 
-employe_info1 = {"name":"Dima","surname":"Bezsonov","salary": "500"} #third
+employe_info1 = {"name":"Dima","surname":"Bezsonov","salary": 500} #third
 print(employe_info1) #trainee
-employe_info1["salary"] = "1500" # after one year
+employe_info1["salary"] *= 1.5 # after half year
 print(employe_info1) # good job
