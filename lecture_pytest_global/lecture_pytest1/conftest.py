aac3ad3902404c050f259b5d25c890a0491d9f40
swollow <swollow@ee.evoplay.tech>
2023-08-27 14:24:16 +0300

    ended home work

--- lecture_pytest_global/lecture_pytest1/conftest.py
+++ lecture_pytest_global/lecture_pytest1/conftest.py
@@ -7,3 +7,9 @@ def test_fixture_1():
     print("global lecture_pytest_fixture_1 fixture setup")
     yield
     print("global lecture_pytest_fixture_1 fixture teardown")
+
+
+@pytest.fixture(autouse=False)
+def test_fixture_2():
+    print("return type of 5 fixture")
+    return 5
--- lecture_pytest_global/lecture_pytest1/test_1.py
+++ lecture_pytest_global/lecture_pytest1/test_1.py
@@ -4,3 +4,7 @@ def test_1():
 
 def test_2(test_fixture_global):
     print("lecture_pytest_test_2 test executed")
+
+
+def test_3(test_fixture_global,test_fixture_2):
+    print(type(test_fixture_2))
